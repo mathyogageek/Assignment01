71b2c90c83c54012fd5a46ed9cdf7597fa152bff
Rie Wilsterman <riewilsterman@Mac-mini.local>
2023-06-20 20:31:48 -0700

    Update script.js content

---
+++ script.js
@@ -0,0 +1,24 @@
+// Step 1
+console.log("This is Step 1");
+
+// Step 2
+function stepTwo() {
+  console.log("This is Step 2");
+}
+
+// Step 3
+const stepThreeElement = document.querySelector("div");
+stepThreeElement.addEventListener("click", () => {
+  console.log("This is Step 3");
+});
+
+// Step 4
+function stepFour() {
+  console.log("This is Step 4");
+}
+
+// Step 5
+const stepFiveElement = document.querySelector("img");
+stepFiveElement.addEventListener("mouseover", () => {
+  console.log("This is Step 5");
+});
