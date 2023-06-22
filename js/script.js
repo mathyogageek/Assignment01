7c6b53323909bbea7a69f0e02510a939c8168fd1
Rie Wilsterman <riewilsterman@Mac-mini.local>
2023-06-21 19:12:47 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Step 1
+
 console.log("This is Step 1");
 
 let someMonth;
@@ -8,23 +8,23 @@ let summerMonth;
 let myLibraryFunction;
 
 
-// Step 2
+
 function stepTwo() {
   console.log("This is Step 2");
 }
 
-// Step 3
+
 const stepThreeElement = document.querySelector("div");
 stepThreeElement.addEventListener("click", () => {
   console.log("This is Step 3");
 });
 
-// Step 4
+
 function stepFour() {
   console.log("This is Step 4");
 }
 
-// Step 5
+
 const stepFiveElement = document.querySelector("img");
 stepFiveElement.addEventListener("mouseover", () => {
   console.log("This is Step 5");
